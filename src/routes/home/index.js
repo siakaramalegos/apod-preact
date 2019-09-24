99c52144eeaf353412ec9cadb0e408f880198f61
Sia <siakaramalegos@gmail.com>
2019-09-24 15:28:28 -0500

    Select another date to show APOD for

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,7 +7,8 @@ const BASE_URL = 'https://api.nasa.gov/planetary/apod'
 
 class Home extends Component {
 	state = {
-		apod: {}
+		apod: {},
+		date: '',
 	}
 
 	componentDidMount() {
@@ -16,10 +17,28 @@ class Home extends Component {
 			.then( data => this.setState({ apod: data }))
 	}
 
+	onInput = (e) => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+
+	changeDate = (e) => {
+		e.preventDefault()
+		fetch(`${BASE_URL}?api_key=${API_KEY}&date=${this.state.date}`)
+			.then(res => res.json())
+			.then(data => this.setState({ apod: data }))
+	}
+
 	render(props, { apod }) {
 		return (
 			<div class={style.home}>
 				<h1>Astronomy Picture of the Day</h1>
+				<form onSubmit={this.changeDate}>
+					<label>
+						Select another date
+						<input type="date" name="date" onInput={this.onInput} />
+					</label>
+					<button>Submit</button>
+				</form>
 				<Apod data={apod} />
 			</div>
 		)
